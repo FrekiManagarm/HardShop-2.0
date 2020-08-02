3de28b789d544da2f0d5562328e160d90cb22e5b
Freki <mathieu.chambaud@ynov.com>
2020-08-02 18:25:50 +0200

    ajout de react-router-dom

---
+++ app-react/src/components/AppRouter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Switch } from ''
+
+class AppRouter extends React.Component {
+    render() {
+        return(
+            
+        )
+    }
+}
+
+export default AppRouter
---
+++ app-react/src/components/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Welcome extends React.Component {
+    render() {
+        return(
+            <div>
+                Bienvenue sur le site du HardShop en Version 2.0
+            </div>
+        )
+    }
+}
+
+export default Welcome
