ef8d3f2e88605cfeffa11fea86c4b94bbc2d9865
Freki <mathieu.chambaud@ynov.com>
2020-08-20 09:26:53 +0200

    ajout des pages du crud

---
+++ app-react/src/components/CRUD/PostBoitier.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class PostBoitier extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+
+        }
+    }
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default PostBoitier
