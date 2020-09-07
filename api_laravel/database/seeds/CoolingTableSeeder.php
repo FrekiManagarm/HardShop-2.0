3c9965e54bd9600baa8ef4b62496a8e5b06e230f
Freki <mathieu.chambaud@ynov.com>
2020-09-07 14:39:23 +0200

    ajout seeder

---
+++ api_laravel/database/seeds/CoolingTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CoolingTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
